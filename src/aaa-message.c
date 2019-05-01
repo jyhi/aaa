91edc96656aa01a9b386d5aa1381a1d65b7dc7b8
YvonneTech <47659990+YvonneTech@users.noreply.github.com>
2019-05-01 16:48:06 +0800

    aaa-message.c: add unfinished deserialize()
    Where can I get the cert? 
    There are still some bugs in aaa_message_deserialize( ) when the input parameter msg is "/hello", but I didn't figure out why.

--- src/aaa-message.c
+++ src/aaa-message.c
@@ -64,13 +64,30 @@ char *aaa_message_serialize(const struct AaaMessage * const Aaa_msg) {
 	return msg;
 }
 
+struct AaaMessage *aaa_message_deserialize(const char * const message_str) {
+	struct AaaMessage messagePacket;
+
+	if (strncmp(message_str, "/hello", 6) == 0)
+	{
+		messagePacket.type = AAA_MESSAGE_TYPE_HELLO;
+		messagePacket.id = get_ip();
+		messagePacket.cert = /* Where can get the cert? */;
+	} else if (strncmp(message_str, "/bye", 4) == 0) {
+		messagePacket.type = AAA_MESSAGE_TYPE_BYE;
+	} else {
+		messagePacket.type = AAA_MESSAGE_TYPE_MSG;
+		messagePacket.message = message_str;
+	}
+	return &messagePacket;
+}
+
 // for testing
 // int main() {
 // 	struct AaaMessage test;
 // 	test.type = AAA_MESSAGE_TYPE_MSG;
 // 	test.id = get_ip();
 // 	test.message = "May the fourth be with you!";
-// 	// test aaa_message_deserialize 
+// 	// test aaa_message_serialize
 // 	// -> mgs
 // 	char *test_msg_ser = aaa_message_serialize(&test);
 // 	printf("%s\n", test_msg_ser);
@@ -82,5 +99,26 @@ char *aaa_message_serialize(const struct AaaMessage * const Aaa_msg) {
 // 	test.type = AAA_MESSAGE_TYPE_BYE;
 // 	test_msg_ser = aaa_message_serialize(&test);
 // 	printf("%s\n", test_msg_ser);
+
+// 	// test aaa_message_deserialize
+// 	char *msg;
+// 	// -> msg
+// 	msg = "How's it going?";
+// 	struct AaaMessage *test_msg_des1 = aaa_message_deserialize(msg);
+// 	printf("%s\n",test_msg_des1->message);
+// 	printf("Type is: %u\n\n",test_msg_des1->type);
+	
+// 	// ->hello
+// 	msg = "/hello";
+// 	struct AaaMessage *test_msg_des2 = aaa_message_deserialize(msg);
+// 	printf("Type is: %u\n",test_msg_des2->type);
+// 	printf("ID is: %s\n",test_msg_des2->id);
+// 	printf("Cert is: %s\n\n",test_msg_des2->cert);
+	
+// 	// -> bye
+// 	msg = "/bye";
+// 	struct AaaMessage *test_msg_des3 = aaa_message_deserialize(msg);
+// 	printf("Type is: %u\n",test_msg_des3->type);
+
 // 	return 0;
 // }
