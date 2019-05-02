57b12e4acfdf057e42b6c99f5eef143dcb5e75f2
YvonneTech <47659990+YvonneTech@users.noreply.github.com>
2019-05-03 00:27:07 +0800

    aaa-packet.c: add string split function
    Solved the problem that if any component of aaa_packet is empty.

--- src/aaa-packet.c
+++ src/aaa-packet.c
@@ -1,5 +1,38 @@
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "aaa-packet.h"
+
+char *str_split(char *str, const char *delim) {
+    static char* left = NULL;
+    if(str == NULL) str = left;
+    
+    const char *p;
+    bool flag;
+    char *s = str;
+    while(*s != 0)
+    {
+        p = delim;
+        flag = false;
+        while(*p != 0)
+        {
+            if(*p++ == *s)
+            {
+                flag = true;
+                *s = 0;
+                break;
+            }
+        }
+        ++s;
+        if(flag)
+        {
+            left = s;
+            return str;
+        }
+    }
+    left = NULL;
+    return str;
+}
 
 char *aaa_packet_serialize(const struct AaaPacket packet) {
     size_t packet_length = packet.message_length + packet.nonce_length + packet.signature_length;
@@ -21,26 +54,26 @@ char *aaa_packet_serialize(const struct AaaPacket packet) {
 }
 
 struct AaaPacket *aaa_packet_deserialize(const char * const packet_str) {
-	struct AaaPacket res;
+	struct AaaPacket *res = (struct AaaPacket *)malloc(sizeof(struct AaaPacket));
 	char *token;
 	
 	// Check whether the string starts with "AAA"
-	token = strtok(packet_str, "!");
+	token = str_split(packet_str, "!");
 	if (strncmp(token, "AAA", 3) != 0) return NULL;
 	
 	// Extract message from the packet_str
-	token = strtok(NULL, ";");
-	res.message = strcat(token, "\0");
+	token = str_split(NULL, ";");
+	res->message = strcat(token, "\0");
 	
 	// Extract nonce from the packet_str
-	token = strtok(NULL, ",");
-	res.nonce = strcat(token, "\0");
+	token = str_split(NULL, ",");
+	res->nonce = strcat(token, "\0");
 	
 	// Extract signature from the packet_str
-	token = strtok(NULL, ".");
-	res.signature = strcat(token, "\0");
+	token = str_split(NULL, ".");
+	res->signature = strcat(token, "\0");
 	
-	return &res;
+	return res;
 }
 
 
