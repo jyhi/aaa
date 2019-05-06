81a467a0d88f37d92481743a2a88159c05b8dfb4
Yvonne <47659990+YvonneTech@users.noreply.github.com>
2019-05-06 21:55:54 +0800

    aaa-message.c: correct message_deserialize() and add JSON_extract()

--- src/aaa-message.c
+++ src/aaa-message.c
@@ -5,22 +5,52 @@
 #include "aaa-message.h"
 
 #define BUFSIZE 2048
+#define TYPE 0
+#define ID 1
+#define CERT 2
+#define MSG 3
 
-void aaa_message_free(struct AaaMessage *message) {
-    if (message->id)
-        free(message->id);
+static const char *type_prefix                 = "type: ";
+static const char *id_prefix                 = "id: ";
+static const char *cert_prefix                 = "cert: ";
+static const char *msg_prefix                 = "msg: ";
 
-    if (message->type == AAA_MESSAGE_TYPE_HELLO) {
-        if (message->cert)
-            free(message->cert);
-    } else if (message->type == AAA_MESSAGE_TYPE_MSG) {
-        if (message->message)
-            free(message->message);
-    } else if (message->type == AAA_MESSAGE_TYPE_BYE) {
-        // nothing to do
-    } else {
-        g_warning("aaa_message_free: unknown type inspected, this is a bug.");
+static char *JSON_extract(const char * const message_str, int infro) {
+    char *buffer = g_malloc0(BUFSIZE);
+    const char *line = NULL;
+
+    switch (infro) {
+        case TYPE:
+            line = g_strstr_len(message_str, -1, type_prefix);
+            if (line) {
+                sscanf(line, "%*s %s", buffer);
+            }
+            break;
+        case ID:
+            line = g_strstr_len(message_str, -1, id_prefix);
+            if (line) {
+                sscanf(line, "%*s %s", buffer);
+            }
+            break;
+        case CERT:
+            line = g_strstr_len(message_str, -1, cert_prefix);
+            if (line) {
+                sscanf(line, "%*s %s", buffer);
+            }
+            break;
+        case MSG:
+            line = g_strstr_len(message_str, -1, msg_prefix);
+            if (line) {
+                sscanf(line, "%*s %s", buffer);
+            }
+            break;
+        default:
+            break;
     }
+    char *ret = g_strdup(buffer);
+
+    g_free(buffer);
+    return ret;
 }
 
 char *aaa_message_serialize(const struct AaaMessage * const Aaa_msg) {
@@ -30,36 +60,36 @@ char *aaa_message_serialize(const struct AaaMessage * const Aaa_msg) {
     strcpy(json_msg, "{\n");
     switch (Aaa_msg->type) {
         case AAA_MESSAGE_TYPE_HELLO:
-            strcat(json_msg, "\t\"type\": \"hello\",\n");
-            strcat(json_msg, "\t\"id\": \"");
+            strcat(json_msg, "type: hello ,\n");
+            strcat(json_msg, "id: ");
             if (Aaa_msg->id)
             {
                 strcat(json_msg, Aaa_msg->id);
             } else {
                 return NULL;
             }
-            strcat(json_msg, "\",\n\t\"cert\": \"");
+            strcat(json_msg, " ,\ncert: ");
             if (Aaa_msg->cert)
             {
                 strcat(json_msg, Aaa_msg->cert);
             } else {
                 return NULL;
             }
-            strcat(json_msg, "\"\n");
+            strcat(json_msg, "\n");
             break;
         case AAA_MESSAGE_TYPE_MSG:
-            strcat(json_msg, "\t\"type\": \"msg\",\n");
-            strcat(json_msg, "\t\"msg\": \"");
+            strcat(json_msg, "type: msg ,\n");
+            strcat(json_msg, "msg: ");
             if (Aaa_msg->message)
             {
                 strcat(json_msg, Aaa_msg->message);
             } else {
                 return NULL;
             }
-            strcat(json_msg, "\"\n");
+            strcat(json_msg, "\n");
             break;
         case AAA_MESSAGE_TYPE_BYE:
-            strcat(json_msg, "\t\"type\": \"bye\"\n");
+            strcat(json_msg, "type: bye\n");
             break;
         default:
             return NULL;
@@ -70,60 +100,24 @@ char *aaa_message_serialize(const struct AaaMessage * const Aaa_msg) {
 
 struct AaaMessage *aaa_message_deserialize(const char * const message_str) {
     if (!message_str) return NULL;
+
     struct AaaMessage *messagePacket = (struct AaaMessage *)malloc(sizeof(struct AaaMessage));
-    
-    if (strncmp(message_str, "/hello", 6) == 0)
+    char *type = JSON_extract(message_str, TYPE);
+
+    if (strncmp(type, "hello", 5) == 0)
     {
         messagePacket->type = AAA_MESSAGE_TYPE_HELLO;
-        messagePacket->id = "temp_id"; // Where can get the name of sender?
-        messagePacket->cert = "temp_cert"; // Where can get the cert?
-    } else if (strncmp(message_str, "/bye", 4) == 0) {
+        messagePacket->id = JSON_extract(message_str, ID);
+        messagePacket->cert = JSON_extract(message_str, CERT);
+    } else if (strncmp(type, "msg", 3) == 0) {
+        messagePacket->type = AAA_MESSAGE_TYPE_MSG;
+        messagePacket->message = JSON_extract(message_str, MSG);
+    } else if (strncmp(type, "bye", 3) == 0) {
         messagePacket->type = AAA_MESSAGE_TYPE_BYE;
     } else {
-        messagePacket->type = AAA_MESSAGE_TYPE_MSG;
-        messagePacket->message = message_str;
+        return NULL;
     }
+
+    free(type);
     return messagePacket;
 }
-
-// for testing
-// int main() {
-// 	struct AaaMessage test;
-// 	test.type = AAA_MESSAGE_TYPE_MSG;
-// 	test.id = get_ip();
-// 	test.message = "May the fourth be with you!";
-// 	// test aaa_message_serialize
-// 	// -> mgs
-// 	char *test_msg_ser = aaa_message_serialize(&test);
-// 	printf("%s\n", test_msg_ser);
-// 	// -> hello
-// 	test.type = AAA_MESSAGE_TYPE_HELLO;
-// 	test_msg_ser = aaa_message_serialize(&test);
-// 	printf("%s\n", test_msg_ser);
-// 	// -> bye
-// 	test.type = AAA_MESSAGE_TYPE_BYE;
-// 	test_msg_ser = aaa_message_serialize(&test);
-// 	printf("%s\n", test_msg_ser);
-
-// 	// test aaa_message_deserialize
-// 	char *msg;
-// 	// -> msg
-// 	msg = "How's it going?";
-// 	struct AaaMessage *test_msg_des1 = aaa_message_deserialize(msg);
-//    	printf("Type is: %u\n",test_msg_des1->type);
-//    	printf("%s\n\n",test_msg_des1->message);
-	
-// 	// ->hello
-// 	msg = "/hello";
-// 	struct AaaMessage *test_msg_des2 = aaa_message_deserialize(msg);
-// 	printf("Type is: %u\n",test_msg_des2->type);
-// 	printf("ID is: %s\n",test_msg_des2->id);
-// 	printf("Cert is: %s\n\n",test_msg_des2->cert);
-	
-// 	// -> bye
-// 	msg = "/bye";
-// 	struct AaaMessage *test_msg_des3 = aaa_message_deserialize(msg);
-// 	printf("Type is: %u\n",test_msg_des3->type);
-
-// 	return 0;
-// }
